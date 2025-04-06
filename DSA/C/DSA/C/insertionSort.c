5f47ca8f64ec09d62ccb00e750e1c75bda1bbf44
114bft68 <168547332+114bft68@users.noreply.github.com>
2025-04-06 17:21:08 +0800

    .

---
+++ DSA/C/DSA/C/insertionSort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define bye(s) ({ \
+    puts(s);      \
+    return 1;     \
+})
+
+void insertionSort(int* array, size_t size);
+
+int main(int argc, char** argv) {
+
+    if (argc <= 2)
+    {
+        printf("No sorting is needed\nFormat: %s <numbers seperated by spaces>\n", argv[0]);
+        return 1;
+    }
+
+    int* array = malloc((argc - 1) * sizeof(int));
+    if (!array) bye("Memory allocation failed");
+
+    for (int i = 1; i < argc; ++i) {
+
+        sscanf(argv[i], "%d", &array[i - 1]);
+
+    }
+
+    insertionSort(array, argc - 1);
+
+    for (int i = 0; i < argc - 1; ++i)
+    {
+        printf("%d ", array[i]);
+    }
+
+    puts("");
+
+    return 0;
+}
+
+void insertionSort(int* array, size_t size) {
+
+    for (int i = 1; i < size; ++i) {
+
+        int key = array[i],
+            beforeKey = i - 1;
+
+        while (key < array[beforeKey] && beforeKey > -1)
+        {
+            array[beforeKey + 1] = array[beforeKey];
+            --beforeKey;
+        }
+
+        array[beforeKey + 1] = key;
+
+    }
+
+}
