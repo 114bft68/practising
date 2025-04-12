abb39bae233488a472a918b35c6864c0ed0bc3f5
114bft68 <168547332+114bft68@users.noreply.github.com>
2025-04-12 22:58:49 +0800

    Add files via upload

---
+++ C/for fun/preprocessor abuse.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define Hello int main(void) {
+#define Bye   }
+
+#define C
+#define Also
+
+#define thanks }
+
+#define repeat for (int i = 0; i < 
+#define times  ; ++i) {
+
+#define Write   puts(
+#define writing puts(
+#define please  );
+
+
+
+Hello C
+Write "something" please
+Also repeat 10 times writing "hello" please thanks
+Bye
